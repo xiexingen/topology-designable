280992f35d02a3806ce5456698602a46e42d1df6
xiexingen <1002275364@qq.com>
2024-01-01 17:50:26 +0800

    docs:deploy

--- src/contexts/x6-react-portal.ts
+++
@@ -1,6 +0,0 @@
-import { Portal } from "@antv/x6-react-shape";
-
-// 注意，一个 graph 只能申明一个 portal provider
-const x6ReactPortalProvider = Portal.getProvider();
-
-export default x6ReactPortalProvider;
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export type { Topology } from './types/global'
-
-export { default as Editor } from './Editor/index'
-
-export { default as Preview } from './Preview/index'
-
-export { default as defaultPropsSchema } from './constants/default-props-schema';
