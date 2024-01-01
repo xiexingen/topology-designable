3bdc004b589d2367d14d7ac99bd2e5af35176515
xiexingen <1002275364@qq.com>
2024-01-01 20:26:46 +0800

    docs:调整部署更目录

---
+++ .umi.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  base: '/topology-designable/',
+  // publicPath: '/blog/',
+  // 国内提速，部分案例会使用不了(如:web worker部分的案例)
+  // publicPath: 'https://fastly.jsdelivr.net/gh/xiexingen/topology-designable@gh-pages/', // cdn.jsdelivr.net
+  // publicPath: 'https://github.com.cnpmjs.org/xiexingen/blog/tree/gh-pages/',
+  // favicons: [
+  //   "https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png",
+  // ],
+  // mode: 'site',
+  // more config: https://d.umijs.org/config
+  exportStatic: {},
+  // styles: [],
+  // links: [
+  //   {
+  //     rel: 'stylesheet',
+  //     href: 'https://unpkg.com/@alifd/theme-design-pro@0.6.2/dist/next-noreset.min.css',
+  //   },
+  //   { rel: 'stylesheet', href: '/style.css' },
+  // ],
+  // copy: ['CNAME'], // 自定义域名,放public文件夹即可
+  // ssr: {},
+});
