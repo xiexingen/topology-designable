30681d50afea3b3d3e9754726024187170068bdb
谢新根 <xiexingen@qianxin.com>
2024-01-17 15:28:44 +0800

    feat: add prettier import order

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,40 @@
 module.exports = {
-  pluginSearchDirs: false,
-  plugins: [
-    require.resolve('prettier-plugin-organize-imports'),
-    require.resolve('prettier-plugin-packagejson'),
-  ],
-  printWidth: 80,
-  proseWrap: 'never',
-  singleQuote: true,
-  trailingComma: 'all',
-  overrides: [
-    {
-      files: '*.md',
-      options: {
-        proseWrap: 'preserve',
-      },
-    },
-  ],
+    pluginSearchDirs: false,
+    plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+    printWidth: 80,
+    proseWrap: 'never',
+    singleQuote: true,
+    trailingComma: 'all',
+    overrides: [
+        {
+            files: '*.md',
+            options: {
+                proseWrap: 'preserve',
+            },
+        },
+    ],
+    tabWidth: 4,
+    trailingComma: 'all',
+    singleQuote: true,
+    semi: true,
+    importOrder: [
+        '<TYPE>^[./]',
+        '<THIRD_PARTY_TYPES>',
+        '^react',
+        '^lodash/(.*)$',
+        '^classnames$',
+        '^html-to-image$',
+        '^antd$',
+        '^antd-(.*)$',
+        // "^@antv/(.*)$",
+        '^@ant-design(.*)$',
+        '^@formily(.*)$',
+        '^ahooks$',
+        '^@/(.*)$',
+        '^../(.*)$',
+        '^./(.*)$',
+        '^[./]',
+    ],
+    // importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
 };
